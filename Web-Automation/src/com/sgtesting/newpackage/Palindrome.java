9c06d3aef9d93079c211b3f254acdaa4e414d3f6
Bhaskarharohalli <hnbhaskar11@gmail.com>
2022-01-13 08:00:23 +0530

    Palindrome

---
+++ Web-Automation/src/com/sgtesting/newpackage/Palindrome.java
@@ -0,0 +1,21 @@
+package com.sgtesting.newpackage;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		String str="Radar", reverseStr="";
+		int strLength=str.length();
+		for(int i=(strLength-1);i>=0;i--)
+		{
+			reverseStr=reverseStr+str.charAt(i);
+		}
+		if(str.toLowerCase().equals(reverseStr.toLowerCase())) {
+			System.out.println(str+" is a palindrome");
+		}
+		else
+		{
+			System.out.println(str+" is a not palindrome");
+		}
+	}
+
+}
