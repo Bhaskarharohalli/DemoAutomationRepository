f277cb877e19889355067b8a68b3cf52d86aba5b
Bhaskarharohalli <hnbhaskar11@gmail.com>
2022-01-18 09:01:29 +0530

    Level-1 frame work

---
+++ ActiTime-Automation-Level1/src/com/sgtesting/actitime/tests/Customers.java
@@ -0,0 +1,100 @@
+package com.sgtesting.actitime.tests;
+
+import org.openqa.selenium.By;
+
+public class Customers extends Initialize{
+	
+	/**
+	 * Created By:
+	 * Created Date:
+	 * Test case ID:
+	 * Reviewed By:
+	 * Reviewed Date:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void createCustomer()
+	{
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id=\"topnav\"]/tbody/tr/td[3]/a")).click();
+			oBrowser.findElement(By.xpath("//div[text()='Add New']")).click();
+			oBrowser.findElement(By.xpath("/html/body/div[14]/div[1]")).click();
+			Thread.sleep(5000);
+			oBrowser.findElement(By.xpath("//*[@id=\"customerLightBox_nameField\"]")).sendKeys("anil");
+			oBrowser.findElement(By.xpath("//*[@id=\'customerLightBox_commitBtn\']/div/span")).click();
+			
+			Thread.sleep(3000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+
+	/**
+	 * Created By:
+	 * Created Date:
+	 * Test case ID:
+	 * Reviewed By:
+	 * Reviewed Date:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void modifyCustomer()
+	{
+		try
+   	 {
+   	  
+   	  oBrowser.findElement(By.xpath("//*[@id=\'cpTreeBlock\']/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/div[2]/div[4]")).click();
+   	  Thread.sleep(5000);
+   	 oBrowser.findElement(By.xpath("//*[@id=\'taskListBlock\']/div[2]/div[2]/div[1]/div[2]/div[1]/div[3]/div/div[1]")).click();
+   	 Thread.sleep(3000);
+   	  oBrowser.findElement(By.xpath("//div[text()='Archived']")).click();
+   
+   	  Thread.sleep(400);
+   	  
+   	 }catch(Exception e)
+   	 {
+   		 e.printStackTrace();
+   	 }
+   	 
+	}
+
+	/**
+	 * Created By:
+	 * Created Date:
+	 * Test case ID:
+	 * Reviewed By:
+	 * Reviewed Date:
+	 * Parameters:
+	 * Return Value:
+	 * Purpose:
+	 * Description:
+	 */
+	public static void deleteCustomer()
+	{
+
+		try
+		{
+			oBrowser.findElement(By.xpath("//*[@id=\"cpTreeBlock\"]/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/div[2]/div[3]")).click();
+			Thread.sleep(2000);
+			oBrowser.findElement(By.xpath("//*[@id=\"cpTreeBlock\"]/div[2]/div[2]/div/div[2]/div/div[1]/div[2]/div[2]/div[4]")).click();
+			Thread.sleep(3000);
+			oBrowser.findElement(By.xpath("//div[text()='ACTIONS']")).click();
+			Thread.sleep(3000);
+			oBrowser.findElement(By.xpath("//*[@id=\'taskListBlock\']/div[2]/div[4]/div/div[3]/div")).click();
+			Thread.sleep(3000);
+			oBrowser.findElement(By.id("customerPanel_deleteConfirm_submitTitle")).click();
+			Thread.sleep(3000);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+
+}
+}
